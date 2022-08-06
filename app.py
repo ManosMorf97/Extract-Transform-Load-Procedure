75679ec59a96c0d190384785667d96d44d6795bc
Morfiadakis Emmanouil <manosmorf97@gmail.com>
2022-08-06 20:32:06 +0300

    FINAL

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask,render_template,url_for,request,redirect
 from connection_to_mongodb import *
 
-def getUserProducts(full_name):
-    db=get_database()
-    collection=db['Users_Products']
-    person=list(collection.find({'full_name':full_name}))[0]
-    return person["products"]
+app = Flask(__name__)
+full_name=""
 
-print(getUserProducts("Albert_Doe"))
+
+def getUserProducts(full_name):
+    try:
+        db=get_database()
+        collection=db['Users_Products']
+        person=list(collection.find({'full_name':full_name}))[0]
+        return person["products"]
+    except:
+        return []
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method=='GET':
+        return render_template('Search_User.html')
+    else:
+        full_name=request.form['search']
+        return redirect(url_for('products',full_name=full_name))
+
+@app.route('/user/<string:full_name>', methods=['GET', 'POST'])
+def products(full_name):
+    if request.method=='GET':
+        products=getUserProducts(full_name)
+        return render_template('Show_Products.html',products=products)
+    else:
+        return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
